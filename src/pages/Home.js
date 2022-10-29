64314743ca8a42774aab0cf23caedad8895ef5d3
AnimeLore <animelorenoreply@gmail.com>
2022-10-29 15:45:20 +0700

    when imposter is sus

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import { TodoList } from '../components/TodoList';
 
-export const Home = () => {
-    return (
-        // eslint-disable-next-line react/react-in-jsx-scope
-        <>
-            {/* eslint-disable-next-line react/react-in-jsx-scope */}
-            <h1 data-testid="page-title">Home</h1>
-            {/* eslint-disable-next-line react/react-in-jsx-scope */}
-            <p>This is the list.</p>
-            {/* eslint-disable-next-line react/react-in-jsx-scope */}
-            <TodoList />
-        </>
-    );
-};
+// eslint-disable-next-line import/prefer-default-export
+export function Home() {
+  return (
+  // eslint-disable-next-line react/react-in-jsx-scope,react/jsx-filename-extension
+    <>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
+      <h1 data-testid="page-title">Home</h1>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
+      <p>This is the list.</p>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
+      <TodoList />
+    </>
+  );
+}
